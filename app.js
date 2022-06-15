bf0fe8f7067b981a575607b571e4c854140a3fba
r26robson <r26robson2hotmail.com>
2022-06-15 11:43:11 -0300

    aletração para o terceiro push

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos, só que não")
 console.log("Aplicação esta se conectando ao banco de dados")
 console.log("Ver a alteração no git pull")
+console.log("terceira auteração para o push")

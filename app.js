247be19fd2b05b5d0fa7004ae0129cbd6eeb45ad
r26robson <72510338+r26robson@users.noreply.github.com>
2022-06-15 11:44:27 -0300

    nova alteração para o pull

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos, só que não")
 console.log("Aplicação esta se conectando ao banco de dados")
 console.log("Ver a alteração no git pull")
-console.log("terceira auteração para o push")
+console.log("terceira alteração para o push")
+console.log("nova alteração para o pull")

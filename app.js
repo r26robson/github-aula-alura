84129fd6cb6710d10da5913066846540688b5a57
r26robson <r26robson2hotmail.com>
2022-06-15 11:59:11 -0300

    voltando ao estado inicial

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
 console.log("Rodando o sistema de gerenciamento de jogos, só que não")
 console.log("Aplicação esta se conectando ao banco de dados")
 console.log("Ver a alteração no git pull")
-console.log("terceira alteração para o push")
-console.log("nova alteração para o pull")
